1eb41ae3f385d8b286faa19d2cbd96cdd1438396
Alexandre Bonneau <alexandre.bonneau@linuxfr.eu>
2017-11-06 14:56:58 -1000

    Redirect to the 404 error page when a specific Snap data cannot be accessed
    Signed-off-by: Alexandre Bonneau <alexandre.bonneau@linuxfr.eu>

--- pages/_user/snap/_snapId.vue
+++ pages/_user/snap/_snapId.vue
@@ -39,59 +39,30 @@
             };
         },
 
-        mounted() {
+        asyncData({ params, store, error }) {
             // Fetch the snap data from the server
-//            console.log(`Getting data from the server...`); //DEBUG
-            this.getSnap();
-        },
-
-        /*
-        async validate({ params }) { //FIXME This does not redirect if the snapId is not found
-            const snapData = await axios.get(`${config.api.baseUrl}/posts/${params.snapId}`);
-            this.snap = snapData;
-            if (snapData.error) {
-                // If the snap ID is not found in the database, redirect to a 404 error page
-                console.log(`An error has been found!`, snapData); //DEBUG
-                return false;
-            }
-
-            return true;
-        },
-        */
-
-        /*
-        async fetch({ store, params }) { //TODO Ideally we would prefer to fetch the data in there instead on in 'mounted'
-            console.log('params.snapId:', params.snapId); //DEBUG
-            console.log('`${config.api.baseUrl}/posts/${params.snapId}`:', `${config.api.baseUrl}/posts/${params.snapId}`); //DEBUG
-            const { data } = await axios.get(`${config.api.baseUrl}/posts/${params.snapId}`);
-            if (data.error) {
-                console.log(`An error has been found!`, data); //DEBUG
-            }
-        },
-        */
-
-        methods: {
-            async getSnap() {
-                return await axios.get(`${config.api.baseUrl}/posts/${this.$route.params.snapId}`)
-                    .then(response => {
-                        this.snap = response.data;
+            return axios.get(`${config.api.baseUrl}/posts/${params.snapId}`)
+                .then(response => {
+                    const snap = response.data;
 //                        console.log(`Snap fetched!`, this.snap); //DEBUG
-                        if (this.snap.error) {
-                            console.error(`Error getting this snap id`); //DEBUG
-                            //FIXME Display a 404 (pending answers from https://github.com/nuxt/nuxt.js/issues/2022)
-                            this.$router.replace({ path: '/404' });
-                        }
-                    },
-                          error => { // Response handler (rejected)
-                              const errorMessage = `Impossible to fetch snap data. Please try again in a moment.`;
-                              console.error(errorMessage, error); //DEBUG
+                    if (snap.error) {
+                        // cf. https://nuxtjs.org/api/context
+                        error({ statusCode: 404, message: 'Error getting this snap id' });
+                    }
 
-                              // Send the event to show a flash message
-                              this.$store.dispatch('snackbar/_showSnackbar', { text: errorMessage, type: 'error' });
+                    return {
+                        snap,
+                    };
+                },
+                      error => { // Response handler (rejected)
+                          const errorMessage = `Impossible to fetch snap data. Please try again in a moment.`;
+                          console.error(errorMessage, error); //DEBUG
 
-                              this.initialFetchFailed = true;
-                          });
-            },
+                          // Send the event to show a flash message
+                          store.dispatch('snackbar/_showSnackbar', { text: errorMessage, type: 'error' });
+
+                          this.initialFetchFailed = true;
+                      });
         },
 
         computed: {
